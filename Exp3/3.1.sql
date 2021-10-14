c442954babe0a9ffce7ff15606b1c258e19d00d3
Bhanu <sunkabhanu281202@gmail.com>
2021-10-15 02:23:46 +0530

    Create cmd

---
+++ Exp3/3.1.sql
@@ -0,0 +1,5 @@
+create table dept57(            -- step 1 .. create table are keywords
+    deptno int(2) primary key,      -- step 2 .. adding columns 
+    ename char(10),
+    location char(10)
+);
