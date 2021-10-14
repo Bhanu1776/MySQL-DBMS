47424af21d1169a6308f6fee262b16a177508128
Bhanu <sunkabhanu281202@gmail.com>
2021-10-15 02:28:35 +0530

    change (alter)

---
+++ Exp3/3.10.sql
@@ -0,0 +1,3 @@
+alter table stud_info
+change address location varchar(10) not null;          -- To change specific column we use alter
+                                                    -- change is a keyword .. it used to rename specific column
